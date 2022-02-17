fde39d8e7b35df3ad18cc2da51a6e05a0c9edec8
DanielUlisses <danielulisses@outlook.com>
2022-02-17 12:15:46 -0300

    Revert "added gke and network"
    This reverts commit 6c7cf443ae589ce02bcb890d81d4c188ca90f00b.

--- infrastructure/modules/iam/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "spinnaker_service_account_email" {
-  value = google_service_account.spinnaker_service_account.email
-}
-
-output "spinnaker_service_account_credentials" {
-  value     = google_service_account_key.spinnaker_service_account.private_key
-  sensitive = true
-}
